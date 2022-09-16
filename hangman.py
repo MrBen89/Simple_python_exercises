e021401a81407ea350af7ac64582ddc78c7577b9
Ben Pearson <b.pearson89@hotmail.com>
2022-09-16 10:22:30 +0900

    Hangman parts 1 and 2

---
+++ hangman.py
@@ -0,0 +1,24 @@
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+
+print(chosen_word)
+
+display = []
+
+for letter in chosen_word:
+    display.append("_")
+
+guess = input("Please guess a letter. ").lower()
+
+count = 0;
+for letter in chosen_word:
+    if guess == letter:
+        display[count] = letter
+        count += 1
+    else:
+        count += 1
+
+print(display)

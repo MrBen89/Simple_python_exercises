d8ae3bf810ccbf13c100e8af7157cde4f0002dd8
Ben Pearson <b.pearson89@hotmail.com>
2022-09-16 11:36:24 +0900

    Step 1 - Create Caeser syphon.

---
+++ caeser.py
@@ -0,0 +1,16 @@
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+direction = input("Type 'encode' to encrypt, or 'decode' to decrypt.")
+
+text = input("Type your message: ").upper()
+shift = int(input("Type the shift number: "))
+
+
+def encrypt(input, shift):
+    message = ""
+    for letter in text:
+        ind = letters.index(letter)
+        message += letters[ind+ shift]
+    print(f"The encoded message is: {message}.")
+
+encrypt(text, shift)

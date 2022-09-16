0318782d5fba5195f8ff8d2d90334c38560df818
Ben Pearson <b.pearson89@hotmail.com>
2022-09-16 11:56:34 +0900

    Final Caeser cypher
    Simplify to single function. Add multiple runs

--- caeser.py
+++ caeser.py
@@ -1,26 +1,28 @@
+import caeser_art
+
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-direction = input("Type 'encode' to encrypt, or 'decode' to decrypt.")
+print(caeser_art.logo)
+def start():
+    direction = input("Type 'encode' to encrypt, or 'decode' to decrypt.")
+    text = input("Type your message: ").upper()
+    shift = int(input("Type the shift number: "))%26
+    caeser(text, shift, direction)
 
-text = input("Type your message: ").upper()
-shift = int(input("Type the shift number: "))
-
-
-def encrypt(input, shift):
+def caeser(text, shift, direction):
     message = ""
     for letter in text:
-        ind = letters.index(letter)
-        message += letters[ind+ shift]
-    print(f"The encoded message is: {message}.")
+        if letter in letters:
+            ind = letters.index(letter)
+            if direction == "encode":
+                message += letters[ind+ shift]
+            elif direction == "decode":
+                message += letters[ind- shift]
+        else:
+             message += letter
+    print(f"The new message is: {message}.")
+    again = input("Do you want to go again? ").lower()
+    if again == "yes":
+        start()
 
-def decrypt(input, shift):
-    message = ""
-    for letter in text:
-        ind = letters.index(letter)
-        message += letters[ind- shift]
-    print(f"The encoded message is: {message}.")
-
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
+start()

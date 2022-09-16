e5673f519e00c8e5ccf941d5c4ec07d4ff2e0f90
Ben Pearson <b.pearson89@hotmail.com>
2022-09-16 10:30:33 +0900

    Update recurssion

--- hangman.py
+++ hangman.py
@@ -11,7 +11,9 @@ display = []
 for letter in chosen_word:
     display.append("_")
 
-while "_" in display:
+end_of_game = false
+
+while not end_of_game:
     guess = input("Please guess a letter. ").lower()
 
     count = 0;
@@ -19,9 +21,12 @@ while "_" in display:
         if guess == letter:
             display[count] = letter
             count += 1
+
         else:
             count += 1
 
     print(display)
+    if "_" not in display:
+        end_of_game = true
 
 print("You win!")

5912e54c56f5e515b31068af692b1238e5f96f15
Ben Pearson <b.pearson89@hotmail.com>
2022-09-16 11:00:10 +0900

    Update hangman
    Final

--- hangman.py
+++ hangman.py
@@ -1,66 +1,25 @@
 import random
+import hangman_words
+import hangman_art
+from os import system, name
 
-word_list = ["aardvark", "baboon", "camel"]
+word_list = hangman_words.word_list
+stages = hangman_art.stages
+logo = hangman_art.logo
 
 chosen_word = random.choice(word_list)
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+def clear():
 
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+print(logo)
 print(chosen_word)
 
 lives = 6
@@ -71,23 +30,35 @@ for letter in chosen_word:
     display.append("_")
 
 end_of_game = False
+guessed = []
 
 while not end_of_game:
+
     guess = input("Please guess a letter. ").lower()
+    if guess not in guessed:
+        guessed.append(guess)
+        count = 0;
+        hit = False
+        for letter in chosen_word:
+            if guess == letter:
+                display[count] = letter
+                count += 1
+                hit = True
+                clear()
 
-    count = 0;
-    hit = False
-    for letter in chosen_word:
-        if guess == letter:
-            display[count] = letter
-            count += 1
-            hit = True
+            else:
+                count += 1
 
-        else:
-            count += 1
+        if hit == False:
+            clear()
+            print(f"Bad luck. The word didnt contain {guess}.")
+            lives -= 1
+    else:
+        clear()
+        print(f"You already guessed {guess}. Please try a  different letter.")
+        print("Letters guessed;")
+        print(guessed)
 
-    if hit == False:
-        lives -= 1
 
     print(display)
     print(stages[lives])

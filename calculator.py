89feb1ba0b1c7280e28decd2c3ea7a602bfc3b0b
Ben Pearson <b.pearson89@hotmail.com>
2022-09-18 08:53:54 +0900

    Calculator recursion added

--- calculator.py
+++ calculator.py
@@ -16,12 +16,24 @@ operations = {
     "*": multiply,
     "/": divide,
 }
+answer = 0
+will_continue = "y"
 
-num1 = int(input("Whats the first number?"))
-num2 = int(input("Whats the second number?"))
-operation = input("Which operator do you want to use? ( +, -, /, *) ")
+while will_continue == "y":
+    if answer == 0:
+        num1 = int(input("Whats the first number?"))
+    else:
+        num1 = answer
+    operation = input("Which operator do you want to use? ( +, -, /, *) ")
+    num2 = int(input("Whats the second number?"))
 
-#Pull the function with the corresponding operator from the dictionary
-answer = operations[operation](num1, num2)
 
-print(f"{num1} {operation} {num2} = {answer}")
+    #Pull the function with the corresponding operator from the dictionary
+    answer = operations[operation](num1, num2)
+
+    print(f"{num1} {operation} {num2} = {answer}")
+
+    will_continue = input("Type 'y' to continue calculating with the previous result, type 'c' to clear, or type 'n' to exit.")
+    if will_continue == "c":
+        answer = 0
+        will_continue = "y"

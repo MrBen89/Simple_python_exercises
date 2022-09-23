3506974e5bf4714e2d2c7e65c06613c9b10c6625
Ben Pearson <b.pearson89@hotmail.com>
2022-09-23 11:26:04 +0900

    Add error handling to Nato encoder

--- nato/main.py
+++ nato/main.py
@@ -1,20 +1,20 @@
 import pandas
 
-
 code_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 code_data_frame = pandas.DataFrame(code_data)
-
-
 
 code ={}
 for (index, row) in code_data.iterrows():
     code[row.letter] = row.code
 
+def decode():
+    input_word = input("Please enter a word: ").upper()
+    try:
+        result = [code[letter] for letter in input_word]
+        except KeyError:
+        print("Sorry, only letters please.")
+        decode()
+    else:
+        print(result)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
-input_word = input("Please enter a word: ").upper()
-
-result = [code[letter] for letter in input_word]
-
-print(result)
+decode()

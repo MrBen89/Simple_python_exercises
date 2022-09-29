1edbfefec566ed9c39aeda3e09de6608ccefe0bf
Ben Pearson <b.pearson89@hotmail.com>
2022-09-29 11:07:33 +0900

    Selenium - auto email signup

--- selenium/main.py
+++ selenium/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path = "C:\Dev\chromedriver.exe"
 
@@ -8,10 +9,15 @@ driver= webdriver.Chrome(executable_path=chrome_driver_path)
 event_x_path = '//*[@id="articlecount"]/a[1]'
 
 
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
+driver.get("http://secure-retreat-92358.herokuapp.com/")
 
-articles = driver.find_element(By.XPATH, event_x_path).text
+#articles = driver.find_element(By.XPATH, event_x_path)
+#articles.click()
 
-print(articles)
-
-driver.quit()
+search = driver.find_element(By.NAME, "fName")
+search.send_keys("Bob")
+search = driver.find_element(By.NAME, "lName")
+search.send_keys("Bobington")
+search = driver.find_element(By.NAME, "email")
+search.send_keys("Bob@Bobington.com")
+search.send_keys(Keys.ENTER)

c7bfc01d45dc9ddb900f1fcbc3d04b732725d51a
Ben Pearson <b.pearson89@hotmail.com>
2022-09-29 12:21:58 +0900

    Create cookie clicker bot

--- selenium/main.py
+++ selenium/main.py
@@ -3,21 +3,40 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 chrome_driver_path = "C:\Dev\chromedriver.exe"
+UPGRADES = 10
+PRODUCTS = 18
 
 driver= webdriver.Chrome(executable_path=chrome_driver_path)
 
 event_x_path = '//*[@id="articlecount"]/a[1]'
 
 
-driver.get("http://secure-retreat-92358.herokuapp.com/")
+driver.get("https://orteil.dashnet.org/cookieclicker/")
 
 #articles = driver.find_element(By.XPATH, event_x_path)
 #articles.click()
 
-search = driver.find_element(By.NAME, "fName")
-search.send_keys("Bob")
-search = driver.find_element(By.NAME, "lName")
-search.send_keys("Bobington")
-search = driver.find_element(By.NAME, "email")
-search.send_keys("Bob@Bobington.com")
-search.send_keys(Keys.ENTER)
+
+running = 1
+while running:
+    cookie = driver.find_element(By.ID, "bigCookie")
+    for _ in range(0,500):
+        try:
+            cookie.click()
+        except:
+            pass
+
+    for num in range(0, UPGRADES+1):
+
+        try:
+            upgrade = driver.find_element(By.ID, f"upgrade{UPGRADES-num}")
+            upgrade.click()
+        except:
+            pass
+    for num in range(0, PRODUCTS+1):
+
+        try:
+            upgrade = driver.find_element(By.ID, f"product{PRODUCTS-num}")
+            upgrade.click()
+        except:
+            pass

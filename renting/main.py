85e5e9c6b634f9e25e00f4b0e6a77afe71dc59ce
Ben Pearson <b.pearson89@hotmail.com>
2022-09-30 11:25:23 +0900

    Create auto data entry

---
+++ renting/main.py
@@ -0,0 +1,47 @@
+
+zillo_url = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D"
+
+from bs4 import BeautifulSoup
+import requests
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+chrome_driver_path = "C:\Dev\chromedriver.exe"
+driver= webdriver.Chrome(executable_path=chrome_driver_path)
+
+
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
+response = requests.get(url=zillo_url, headers=header)
+
+webpage = response.text
+
+soup = BeautifulSoup(webpage, "html.parser")
+#prices = soup.find_all("span", {"data-test": "property-card-price"})
+javascript_data = soup.find('script', {'data-zrr-shared-data-key': 'mobileSearchPageStore'}).text
+
+javascript_data = soup.find('script', {'data-zrr-shared-data-key': 'mobileSearchPageStore'}).text
+json_data = json.loads(javascript_data.split('--')[1])
+
+
+print(json_data['cat1']['searchResults']['listResults'][0])
+count=0
+for data in json_data:
+    driver.get("LINK")
+    time.sleep(1)
+    address = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    address.send_keys(json_data['cat1']['searchResults']['listResults'][count]["address"])
+    price = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price.send_keys(json_data['cat1']['searchResults']['listResults'][count]["units"][0]["price"])
+    url = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    url.send_keys(json_data['cat1']['searchResults']['listResults'][count]["detailUrl"])
+    submit = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+    submit.click()
+    count +=1
+    time.sleep(1)

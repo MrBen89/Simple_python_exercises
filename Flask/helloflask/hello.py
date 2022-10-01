f07cb213e68669dfb4ce4640a1c096f8e904f9ca
Ben Pearson <b.pearson89@hotmail.com>
2022-10-01 09:40:29 +0900

    Create high low guess game

--- Flask/helloflask/hello.py
+++ Flask/helloflask/hello.py
@@ -1,9 +1,33 @@
 from flask import Flask
 app = Flask(__name__)
+import random
+
+number = random.randint(1,9)
+
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
 
 @app.route("/")
-def hello_world():
-    return "Hello World"
+def home():
+
+    return "<h1> Guess a number between 1 and 9 </h1>"\
+            "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>"
+
+@app.route("/<int:num>")
+def check(num):
+    if number == num:
+        return "<h1> That's Right! </h1>"\
+                "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'>"
+    elif number > num:
+        return "<h1> Too low! </h1>"\
+                "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'>"
+    elif number < num:
+        return "<h1> Too high! </h1>"\
+                "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'>"
+
+
 
 if __name__ == "__main__":
     app.run()

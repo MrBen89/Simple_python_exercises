b1291a432444f8c71002707bf76ff4ea2a0adf10
Ben Pearson <b.pearson89@hotmail.com>
2022-10-02 09:34:52 +0900

    create simple blog

--- Flask/blog/server.py
+++ Flask/blog/server.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template
 import requests
 
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    response = requests.get("https://api.npoint.io/b3a3863991f5dd01b761")
+    all_posts = response.json()
+    print(all_posts)
+    return render_template("index.html", posts=all_posts)
 
-@app.route("/guess/<name>")
-def person(name):
-    response = requests.get(f"https://api.genderize.io?name={name}")
-    gender = response.json()["gender"]
-    response = requests.get(f"https://api.agify.io?name={name}")
-    age = response.json()["age"]
-    return render_template("index.html", name=name, gender=gender, age=age)
+@app.route("/blog/<id>")
+def get_blog(id):
+    response = requests.get("https://api.npoint.io/b3a3863991f5dd01b761")
+    post = response.json()[int(id)]
+    return render_template("post.html", post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)

b706538cbd8880b31d58ab90a06043573f0f0201
Ben Pearson <b.pearson89@hotmail.com>
2022-10-02 10:27:20 +0900

    Add bootstrap styles to blog

--- Flask/blog/server.py
+++ Flask/blog/server.py
@@ -7,15 +7,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    response = requests.get("https://api.npoint.io/b3a3863991f5dd01b761")
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
     all_posts = response.json()
     print(all_posts)
     return render_template("index.html", posts=all_posts)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 @app.route("/blog/<id>")
 def get_blog(id):
-    response = requests.get("https://api.npoint.io/b3a3863991f5dd01b761")
-    post = response.json()[int(id)]
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    post = response.json()[int(id)-1]
+    print(post)
     return render_template("post.html", post=post)
 
 if __name__ == "__main__":
